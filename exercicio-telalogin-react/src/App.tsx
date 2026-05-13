452827895ca94f560764603aa8e3ded8ac3254b0
Gabriel Oliveira <gabrieloliveirag403@gmail.com>
2026-05-13 12:18:09 -0300

    feat: adiciona exercício tela de login em React

---
+++ exercicio-telalogin-react/src/App.tsx
@@ -0,0 +1,12 @@
+import Login from "./components/Login";
+import "./styles/globals.css";
+
+function App() {
+  return (
+    <section className="bg-black flex h-screen items-center justify-center">
+      <Login></Login>
+    </section>
+  );
+}
+
+export default App;
